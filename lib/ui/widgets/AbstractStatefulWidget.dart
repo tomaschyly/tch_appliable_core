fba4d3a75984d2dae32a524df58930e3282d1f6d
tomaschyly <chyly@tomas-chyly.com>
2020-12-17 21:07:34 +0100

    AbstractStatefulWidget component

---
+++ lib/ui/widgets/AbstractStatefulWidget.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+enum WidgetState {
+  NotInitialized,
+  Initialized,
+  Disposed,
+}
+
+abstract class AbstractStatefulWidget extends StatefulWidget {}
+
+abstract class AbstractStatefulWidgetState<T extends AbstractStatefulWidget> extends State<T> {
+  WidgetState get widgetState => _widgetState;
+
+  WidgetState _widgetState = WidgetState.NotInitialized;
+
+  /// Manually dispose of resources
+  @override
+  void dispose() {
+    _widgetState = WidgetState.Disposed;
+
+    super.dispose();
+  }
+
+  /// Create view layout from widgets
+  @override
+  Widget build(BuildContext context) {
+    if (_widgetState == WidgetState.NotInitialized) {
+      firstBuildOnly(context);
+    }
+
+    return Builder(
+      builder: (BuildContext context) => buildContent(context),
+    );
+  }
+
+  /// Run initializations of screen on first build only
+  @protected
+  firstBuildOnly(BuildContext context) {
+    _widgetState = WidgetState.Initialized;
+  }
+
+  /// Create screen content from widgets
+  @protected
+  Widget buildContent(BuildContext context);
+
+  /// Call setState only if it not disposed yet
+  @protected
+  void setStateNotDisposed(VoidCallback fn) {
+    if (_widgetState != WidgetState.Disposed) {
+      setState(fn);
+    }
+  }
+}
