a61c09407694d6cfd75d09a299cba55eb881a6e1
tomaschyly <chyly@tomas-chyly.com>
2021-01-08 22:09:35 +0100

    Partial SQLiteSource implementation

--- lib/providers/MainDataProvider.dart
+++ lib/providers/MainDataProvider.dart
@@ -224,22 +224,159 @@ class SQLiteOptions {
 
 class SQLiteSource extends AbstractSource {
   final SQLiteOptions _options;
+  Database? _database;
 
   /// SQLiteSource initialization
   SQLiteSource({
     required SQLiteOptions options,
-  }) : _options = options;
+  }) : _options = options {
+    state = ValueNotifier(MainDataProviderSourceState.Ready);
+  }
+
+  /// Create Database connection and init tables structure
+  Future<Database> _open() async {
+    Database? database = _database;
+
+    if (database == null) {
+      database = await openDatabase(
+        _options.databasePath,
+        version: _options.version,
+        onCreate: _options.onCreate,
+      );
+
+      _database = database;
+    }
+
+    return database;
+  }
+
+  /// Run any raw SQL command/query on the database
+  Future<void> raw(String query) async {
+    final database = await _open();
+
+    return database.execute(query);
+  }
+
+  /// Query data from database
+  Future<List<Map<String, dynamic>>> query(String table, Map<String, dynamic> parameters, {String? rawQuery, List<dynamic>? rawArguments}) async {
+    final database = await _open();
+
+    if (rawQuery?.isNotEmpty == true) {
+      final List<Map<String, dynamic>> results = await database.rawQuery(rawQuery!, rawArguments);
+
+      return results;
+    }
+
+    String? where;
+    List<dynamic>? whereArgs;
+
+    if (parameters.isNotEmpty) {
+      where = parameters.keys.join(' ');
+      whereArgs = parameters.values.toList();
+    }
+
+    final List<Map<String, dynamic>> results = await database.query(table, where: where, whereArgs: whereArgs);
+
+    return results;
+  }
+
+  /// Insert data into database
+  Future<int> _insert(String table, Map<String, dynamic> data) async {
+    final database = await _open();
+
+    return database.insert(table, data);
+  }
+
+  /// Update data in database
+  Future<int> _update(String table, Map<String, dynamic> data, int id) async {
+    final database = await _open();
+
+    return database.update(table, data, where: 'id = ?', whereArgs: [id]);
+  }
+
+  /// Insert or update depending on if existing already
+  Future<int> save(String table, Map<String, dynamic> data, {int? id}) async {
+    if (id != null && id > 0) {
+      await _update(table, data, id);
+
+      return id;
+    } else {
+      return _insert(table, data);
+    }
+  }
+
+  /// Delete data from database
+  Future<int> delete(String table, int id) async {
+    final database = await _open();
+
+    return database.delete(table, where: 'id = ?', whereArgs: [id]);
+  }
+
+  /// Query data from database and update DataSources
+  Future<void> _queryDataUpdate(DataRequest dataRequest) async {
+    final List<Map<String, dynamic>> results = await query(dataRequest.method, dataRequest.parameters);
+
+    _dataSources.forEach((MainDataSource dataSource) {
+      if (dataSource.identifiers.contains(dataRequest.identifier)) {
+        dataSource.setResult(dataRequest.identifier, <String, dynamic>{
+          'list': results,
+        });
+      }
+    });
+  }
 
   /// Register the DataSource for data
   @override
   registerDataSource(MainDataSource dataSource) {
-    throw Exception('SQLiteSource is not implemented');
+    _dataSources.add(dataSource);
+
+    registerDataRequests(dataSource);
+
+    reFetchData(identifiers: dataSource.identifiers);
   }
 
   /// UnRegister the DataSource from receiving data
   @override
   unRegisterDataSource(MainDataSource dataSource) {
-    throw Exception('SQLiteSource is not implemented');
+    unRegisterDataRequests(dataSource);
+
+    _dataSources.remove(dataSource);
+  }
+
+  /// ReFetch data for DataRequest based on methods or identifiers
+  Future<void> reFetchData({List<String>? methods, List<String>? identifiers}) async {
+    if (methods == null && identifiers == null) {
+      throw Exception('Provide either methods or identifiers');
+    }
+
+    if (methods != null) {
+      final List<String> identifiers = List.empty(growable: true);
+
+      for (String method in methods) {
+        for (MainDataSource dataSource in _dataSources) {
+          final DataRequest? dataRequest = dataSource.requestForMethod(method);
+
+          if (dataRequest != null && !identifiers.contains(dataRequest.identifier)) {
+            identifiers.add(dataRequest.identifier);
+          }
+        }
+      }
+
+      await reFetchData(identifiers: identifiers);
+    }
+
+    if (identifiers != null) {
+      for (String identifier in identifiers) {
+        for (MainDataSource dataSource in _dataSources) {
+          final DataRequest? dataRequest = dataSource.requestForIdentifier(identifier);
+
+          if (dataRequest != null) {
+            await _queryDataUpdate(dataRequest);
+            break;
+          }
+        }
+      }
+    }
   }
 
   /// Check if DataRequest has next page
