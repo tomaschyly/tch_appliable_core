34a1a2aed177dc9a99db5e91483bcc43c6aa0f32
tomaschyly <chyly@tomas-chyly.com>
2021-01-16 12:24:44 +0100

    More useful utils

--- lib/utils/List.dart
+++ lib/utils/List.dart
@@ -7,4 +7,23 @@ extension ListExtension<E> on List<E> {
       return null;
     }
   }
+
+  /// Returns the last element that satisfies the given predicate or null
+  E? lastWhereOrNull(bool test(E element), {E orElse()?}) {
+    try {
+      late E result;
+      bool foundMatching = false;
+      for (E element in this) {
+        if (test(element)) {
+          result = element;
+          foundMatching = true;
+        }
+      }
+      if (foundMatching) return result;
+      if (orElse != null) return orElse();
+      throw new StateError("No element");
+    } catch (e) {
+      return null;
+    }
+  }
 }
---
+++ lib/utils/Numbers.dart
@@ -0,0 +1,34 @@
+import 'dart:ui';
+
+import 'package:intl/intl.dart';
+
+/// If possible convert text value to double number
+double? stringToDouble(String? text) {
+  if (text == null) return null;
+
+  try {
+    final num parsed = NumberFormat.decimalPattern(window.locale.languageCode).parse(text);
+
+    return parsed.toDouble();
+  } catch (e) {}
+
+  return double.tryParse(text);
+}
+
+/// Covert double to string respecting decimal symbol by Locale
+String doubleToString(double value) {
+  try {
+    return NumberFormat.decimalPattern(window.locale.languageCode).format(value);
+  } catch (e) {}
+
+  return value.toString();
+}
+
+/// Convert double to currency text formatted with decimals
+String doubleAsCurrency(double? value) {
+  try {
+    return NumberFormat.decimalPattern(window.locale.languageCode).format(value ?? 0.0);
+  } catch (e) {}
+
+  return (value ?? 0.0).toStringAsFixed(2).replaceFirst('.00', '');
+}
