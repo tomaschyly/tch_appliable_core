b83c8e9201b1838db4b1665ec18e3c2dcd2d19e0
tomaschyly <chyly@tomas-chyly.com>
2024-03-20 12:23:12 +0100

    Replace deprecated use of global window for numbers

--- lib/utils/numbers.dart
+++ lib/utils/numbers.dart
@@ -1,33 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+/// Get default locale from BuildContext
+Locale platformLocale(BuildContext context) {
+  return View.of(context).platformDispatcher.locale;
+}
+
 /// If possible convert text value to double number
-double? stringToDouble(String? text) {
+double? stringToDouble(String? text, [String? languageCode]) {
   if (text == null) return null;
 
   try {
-    final num parsed = NumberFormat.decimalPattern(WidgetsBinding.instance.window.locale.languageCode).parse(text);
+    final num parsed = NumberFormat.decimalPattern(languageCode).parse(text);
 
     return parsed.toDouble();
-  } catch (e) {}
+  } catch (e) {
+    debugPrint(e.toString());
+  }
 
   return double.tryParse(text);
 }
 
 /// Convert double to string respecting decimal symbol by Locale
-String doubleToString(double value) {
+String doubleToString(double value, [String? languageCode]) {
   try {
-    return NumberFormat.decimalPattern(WidgetsBinding.instance.window.locale.languageCode).format(value);
-  } catch (e) {}
+    return NumberFormat.decimalPattern(languageCode).format(value);
+  } catch (e) {
+    debugPrint(e.toString());
+  }
 
   return value.toString();
 }
 
 /// Convert double to currency text formatted with decimals
-String doubleAsCurrency(double? value) {
+String doubleAsCurrency(double? value, [String? languageCode]) {
   try {
-    return NumberFormat.decimalPattern(WidgetsBinding.instance.window.locale.languageCode).format(value ?? 0.0);
-  } catch (e) {}
+    return NumberFormat.decimalPattern(languageCode).format(value ?? 0.0);
+  } catch (e) {
+    debugPrint(e.toString());
+  }
 
   return (value ?? 0.0).toStringAsFixed(2).replaceFirst('.00', '');
 }
